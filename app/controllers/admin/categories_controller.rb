0ea3b832c13872d105f7ad36f44312bd2e01d853
Kai Tang <16kt16@queensu.ca>
2017-11-09 01:34:43 +0000

    categories function done:

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,21 @@
+class Admin::CategoriesController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @category = Category.new(params[:category].permit(:name))
+        
+        if @category.save
+            
+            redirect_to admin_categories_path
+        else
+            render 'new'
+        end
+    end
+
+    def index
+        @categories = Category.all
+    end
+
+end
